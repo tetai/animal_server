0428d6683f4ae541245f3589d0675e0eb8f8df48
zhengkanzhong <zhengkanzhong@meizu.com>
2021-07-26 11:22:25 +0800

    添加websocket聊天功能

---
+++ animal/src/main/java/cn/zkz/animal/ws/webscoket/MyMessage.java
@@ -0,0 +1,23 @@
+package cn.zkz.animal.ws.webscoket;
+
+public class MyMessage {
+
+    private String userId;
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
